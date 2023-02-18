2e2abb7b5ce90e8de5d20144e8eee0516be75617
talhayu <mnhetz999@gmail.com>
2023-02-18 14:20:04 +0500

    show password added

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 document.getElementById("image-container").classList.add("hidden");
-const store = [];
 const imageInput = document.getElementById('image-input');
       const uploadButton = document.getElementById('upload-button');
       const uploadedImage = document.getElementById('uploaded-image');
-
       uploadButton.addEventListener('click', () => {
         const file = imageInput.files[0];
         if (file) {
@@ -15,7 +13,18 @@ const imageInput = document.getElementById('image-input');
           };
         }
       });
+      
+     const check= document.getElementById("check");
+     check.addEventListener('click', () =>{
+        if(check.checked){
+         password.setAttribute("type","text")
+        }
+         else{
+          password.setAttribute("type","password")
+      }
+     })
 
+const store = [];
 function addinput() {
   const name = document.getElementById("name");
   const lastname = document.getElementById("lastname");
@@ -66,22 +75,37 @@ function addinput() {
 
     document.querySelectorAll("#form input").forEach(input => input.classList.add("hidden"));
     document.querySelector("#form button").classList.add("hidden");
+    document.querySelector("#form a").classList.add("hidden");
     document.getElementById("heading").innerHTML="login form"
    
 
     const input1=document.createElement("input");
     const input2=document.createElement("input");
+    const checkbox=document.createElement("input");
+    const checktext=document.createElement("a")
     const button=document.createElement("button");
     const para=document.createElement("p")
 
     button.textContent="submit"
     input1.setAttribute("type","email")
     input2.setAttribute("type","password")
+    checkbox.setAttribute("type","checkbox")
+    checktext.textContent="show password"
     input1.setAttribute("placeholder","enter email")
     input2.setAttribute("placeholder","enter password")
     input1.classList.add("box","email")
     input2.classList.add("box")
+    checktext.classList.add("text")
     button.classList.add("btn")
+    checkbox.onclick=function(){
+      if(checkbox.checked){
+        input2.setAttribute("type","text")
+      }
+      else{
+        input2.setAttribute("type","password")
+      }
+    }
+
     button.onclick=  function () {
 
   if (input1.value === store[2] && input2.value=== store[3]) {
@@ -101,6 +125,8 @@ function addinput() {
    
     document.getElementById("form").appendChild(input1);
     document.getElementById("form").appendChild(input2)
+    document.getElementById("form").appendChild(checkbox)
+    document.getElementById("form").appendChild(checktext)
     document.getElementById("form").appendChild(button)
     document.getElementById("form").appendChild(para)
 
